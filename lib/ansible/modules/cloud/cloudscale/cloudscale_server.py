9e7c7986a64304d478bc1c7f0ad01ff55caa520f
Denis Krienbühl <denis.krienbuehl@cloudscale.ch>
2020-02-04 14:02:51 +0100

    Fixes flavor return value documentation

--- lib/ansible/modules/cloud/cloudscale/cloudscale_server.py
+++ lib/ansible/modules/cloud/cloudscale/cloudscale_server.py
@@ -218,8 +218,8 @@ state:
 flavor:
   description: The flavor that has been used for this server
   returned: success when not state == absent
-  type: str
-  sample: flex-8
+  type: dict
+  sample: { 'slug': 'flex-4', 'name': 'Flex-4', 'vcpu_count': 2, 'memory_gb': 4}
 image:
   description: The image used for booting this server
   returned: success when not state == absent
